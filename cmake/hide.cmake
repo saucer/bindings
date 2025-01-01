7e0297baa2ecf7e5b8c0558172ff618f849dc679
Curve <fynnbwdt@gmail.com>
2025-01-01 19:58:21 +0100

    refactor(cmake): scope symbol visibility

---
+++ cmake/hide.cmake
@@ -0,0 +1,3 @@
+function(saucer_bindings_hide_symbols NAME)
+    set_target_properties(${NAME} PROPERTIES C_VISIBILITY_PRESET hidden CXX_VISIBILITY_PRESET hidden VISIBILITY_INLINES_HIDDEN ON WINDOWS_EXPORT_ALL_SYMBOLS OFF)
+endfunction()
--- cmake/module.cmake
+++ cmake/module.cmake
@@ -1,7 +1,9 @@
 function(saucer_bindings_add_module NAME MACRO)
     message(STATUS "[saucer-bindings] Adding module: ${NAME}")
 
+    saucer_bindings_hide_symbols(${NAME})
     saucer_bindings_export(${NAME} ${MACRO})
+
     get_target_property(priv_includes saucer::bindings INCLUDE_DIRECTORIES)
 
     target_link_libraries(${NAME} PUBLIC saucer::bindings)
